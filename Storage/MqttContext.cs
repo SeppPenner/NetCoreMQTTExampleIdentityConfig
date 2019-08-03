59dde6d46abe45d1c691292361a3e4b23593e404
HansM <HansM2013@gmx.de>
2019-08-03 20:46:42 +0200

    Fixed database context.

--- Storage/MqttContext.cs
+++ Storage/MqttContext.cs
@@ -48,7 +48,7 @@ namespace Storage
         /// </summary>
         public string ConnectionString => this.connectionSettings.ToConnectionString();
 
-        /// <inheritdoc cref="MqttContext"/>
+        /// <inheritdoc cref="IdentityDbContext"/>
         /// <summary>
         ///     Configures the database.
         /// </summary>
@@ -60,6 +60,133 @@ namespace Storage
         {
             optionsBuilder
                 .UseNpgsql($"Host={this.connectionSettings.Host};Database={this.connectionSettings.Database};Username={this.connectionSettings.Username};Password={this.connectionSettings.Password};Port={this.connectionSettings.Port}");
+        }
+
+        /// <inheritdoc cref="IdentityDbContext"/>
+        /// <summary>
+        /// Configures the schema needed for the identity framework.
+        /// </summary>
+        /// <param name="builder">The builder being used to construct the model for this context.</param>
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<User>(b =>
+            {
+                // Primary key
+                b.HasKey(u => u.Id);
+
+                // Indexes for "normalized" username and email, to allow efficient lookups
+                b.HasIndex(u => u.NormalizedUserName).HasName("UserNameIndex").IsUnique();
+                b.HasIndex(u => u.NormalizedEmail).HasName("EmailIndex");
+
+                // Maps to the Users table
+                b.ToTable("Users");
+
+                // A concurrency token for use with the optimistic concurrency checking
+                b.Property(u => u.ConcurrencyStamp).IsConcurrencyToken();
+
+                // Limit the size of columns to use efficient database types
+                b.Property(u => u.UserName).HasMaxLength(256);
+                b.Property(u => u.NormalizedUserName).HasMaxLength(256);
+                b.Property(u => u.Email).HasMaxLength(256);
+                b.Property(u => u.NormalizedEmail).HasMaxLength(256);
+
+                // The relationships between User and other entity types
+                // Note that these relationships are configured with no navigation properties
+
+                // Each User can have many UserClaims
+                b.HasMany<UserClaim>().WithOne().HasForeignKey(uc => uc.UserId).IsRequired();
+
+                // Each User can have many UserLogins
+                b.HasMany<UserLogin>().WithOne().HasForeignKey(ul => ul.UserId).IsRequired();
+
+                // Each User can have many UserTokens
+                b.HasMany<UserToken>().WithOne().HasForeignKey(ut => ut.UserId).IsRequired();
+
+                // Each User can have many entries in the UserRole join table
+                b.HasMany<UserRole>().WithOne().HasForeignKey(ur => ur.UserId).IsRequired();
+            });
+
+            builder.Entity<UserClaim>(b =>
+            {
+                // Primary key
+                b.HasKey(uc => uc.Id);
+
+                // Maps to the UserClaims table
+                b.ToTable("UserClaims");
+            });
+
+            builder.Entity<UserLogin>(b =>
+            {
+                // Composite primary key consisting of the LoginProvider and the key to use
+                // with that provider
+                b.HasKey(l => new { l.LoginProvider, l.ProviderKey });
+
+                // Limit the size of the composite key columns due to common DB restrictions
+                b.Property(l => l.LoginProvider).HasMaxLength(128);
+                b.Property(l => l.ProviderKey).HasMaxLength(128);
+
+                // Maps to the UserLogins table
+                b.ToTable("UserLogins");
+            });
+
+            builder.Entity<UserToken>(b =>
+            {
+                // Composite primary key consisting of the UserId, LoginProvider and Name
+                b.HasKey(t => new { t.UserId, t.LoginProvider, t.Name });
+
+                // Limit the size of the composite key columns due to common DB restrictions
+                b.Property(t => t.LoginProvider).HasMaxLength(256);
+                b.Property(t => t.Name).HasMaxLength(256);
+
+                // Maps to the UserTokens table
+                b.ToTable("UserTokens");
+            });
+
+            builder.Entity<Role>(b =>
+            {
+                // Primary key
+                b.HasKey(r => r.Id);
+
+                // Index for "normalized" role name to allow efficient lookups
+                b.HasIndex(r => r.NormalizedName).HasName("RoleNameIndex").IsUnique();
+
+                // Maps to the Roles table
+                b.ToTable("Roles");
+
+                // A concurrency token for use with the optimistic concurrency checking
+                b.Property(r => r.ConcurrencyStamp).IsConcurrencyToken();
+
+                // Limit the size of columns to use efficient database types
+                b.Property(u => u.Name).HasMaxLength(256);
+                b.Property(u => u.NormalizedName).HasMaxLength(256);
+
+                // The relationships between Role and other entity types
+                // Note that these relationships are configured with no navigation properties
+
+                // Each Role can have many entries in the UserRole join table
+                b.HasMany<UserRole>().WithOne().HasForeignKey(ur => ur.RoleId).IsRequired();
+
+                // Each Role can have many associated RoleClaims
+                b.HasMany<RoleClaim>().WithOne().HasForeignKey(rc => rc.RoleId).IsRequired();
+            });
+
+            builder.Entity<RoleClaim>(b =>
+            {
+                // Primary key
+                b.HasKey(rc => rc.Id);
+
+                // Maps to the RoleClaims table
+                b.ToTable("RoleClaims");
+            });
+
+            builder.Entity<UserRole>(b =>
+            {
+                // Primary key
+                b.HasKey(r => new { r.UserId, r.RoleId });
+
+                // Maps to the UserRoles table
+                b.ToTable("UserRoles");
+            });
         }
     }
 }
